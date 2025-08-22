39a7ff134179f121a764d16f14fc366550e1bbdd
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-08-22 11:36:06 +0500

    Implement point dispersion

--- src/main.js
+++ src/main.js
@@ -7,3 +7,26 @@ const geometry = new THREE.SphereGeometry( 1, 64, 64 )
 const material = new THREE.PointsMaterial( { sizeAttenuation: false, size: 1 } )
 const object = new THREE.Points( geometry, material )
 scene.add( object )
+
+// Velocity
+const velocity = []
+
+for ( let i = 0; i < geometry.attributes.position.count; i++ ) {
+
+	velocity.push( Math.random() - 0.5 )
+}
+
+geometry.setAttribute( "velocity", new THREE.Float32BufferAttribute( velocity, 1 ) )
+
+// Dispersion
+const positionAttribute = geometry.attributes.position
+const velocityAttribute = geometry.attributes.velocity
+const tmp = new THREE.Vector3()
+const scale = 0.5
+
+for( let i = 0; i < positionAttribute.count; i++ ) {
+
+	tmp.fromBufferAttribute( positionAttribute, i )
+	tmp.addScaledVector( tmp, velocityAttribute.getX( i ) * scale )
+	positionAttribute.setXYZ( i, ...tmp )
+}
